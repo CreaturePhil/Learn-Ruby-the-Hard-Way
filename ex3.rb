ee5ccd17be1a0b367fdc30e02a1660af77fff7e0
Phil <birkal@outlook.com>
2014-06-17 23:27:53 -0700

    Exercise 3: Numbers And Math

---
+++ ex3.rb
@@ -0,0 +1,21 @@
+puts "I will now count my chickens:"
+
+puts "Hens", 25 + 30 / 6
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the egss:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 < 5 - 7
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
