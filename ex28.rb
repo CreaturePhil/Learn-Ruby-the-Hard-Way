ed7e9fefd7c189f88b8d224b8ad0646af06571f6
Phil <birkal@outlook.com>
2014-06-19 22:16:36 -0700

    Exercise 28: Boolean Practice

---
+++ ex28.rb
@@ -0,0 +1,20 @@
+true and true
+false and true
+1 == 1 and 2 == 1
+"test" == "test"
+1 == 1 or 2 != 1
+true and 1 == 1
+false and 0 != 0
+true or 1 == 1
+"test" == "testing"
+1 != 0 and 2 == 1
+"test" != "testing"
+"test" == 1
+not (true and false)
+not (1 == 1 and 0 != 1)
+not (10 == 1 or 1000 == 1000)
+not (1 != 10 or 3 == 4)
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+1 == 1 and not ("testing" == 1 or 1 == 0)
+"chunky" == "bacon" and not (3 == 4 or 3 == 3)
+3 == 3 and not ("testing" == "testing" or "Ruby" == "Fun")
